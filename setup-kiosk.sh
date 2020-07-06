5290c2b96267aebff135c4a49735921dcddfdcda
Jean Honlet <jehon@users.noreply.github.com>
2020-07-06 21:38:44 +0200

    feature: setup kiosk

--- setup-kiosk.sh
+++ setup-kiosk.sh
@@ -2,3 +2,13 @@
 
 ./setup-remote.sh kiosk pi
 ./setup-remote-home.sh kiosk
+
+ssh root@kiosk -T << EOS
+	mkdir -p /opt/kiosk
+	cd /opt/kiosk
+	wget https://raw.githubusercontent.com/jehon/packages/master/start -O start
+	chmod +x start
+	./start
+EOS
+
+# TODO: copy fstab file from local repo
