87281bd3a1166bfdea1f3fcb854bf0e556f16921
Jean Honlet <jehon@users.noreply.github.com>
2020-10-06 21:58:38 +0200

    feature: shuttle-go for vlc

--- jehon-base-minimal/usr/bin/jh-shuttle-go.sh
+++ jehon-base-minimal/usr/bin/jh-shuttle-go.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-shuttle-go -c /usr/share/jehon-base-minimal/shuttle-go.json
+# shellcheck source=/dev/null
+. jh-lib.sh
+
+set -e
+
+EXE="/usr/bin/shuttle-go"
+CFG="/usr/share/jehon-base-minimal/shuttle-go.json"
+
+if [ -n "$PKG_FOLDER" ]; then
+    EXE="$PKG_FOLDER/externals/shuttle-go/shuttle-go"
+    CFG="$PKG_FOLDER/jehon-base-minimal/usr/share/jehon-base-minimal/shuttle-go.json"
+fi
+
+echo "EXE: $EXE"
+echo "CFG: $CFG"
+
+"$EXE" \
+    -config "$CFG" \
+    -debug
