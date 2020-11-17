f5829314fdc5a1be78f76177bc905a05d38fbed8
Jean Honlet <jehon@users.noreply.github.com>
2020-11-17 18:00:00 +0100

    feature: jh-synology-mount

---
+++ bin/jh-synology-mount.sh
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env bash
+
+set -e
+
+# shellcheck source=/dev/null
+. jh-lib.sh
+
+TARGET=~/synology/"$1"
+
+mkdir -p "$TARGET"
+sudo mount "//$JH_SYNOLOGY_IP/$1" "$TARGET" \
+    -t cifs \
+    -o vers=1.0,auto,user,noexec,nosuid,credentials=/home/jehon/src/bin/synology.credentials,iocharset=utf8,cache=strict
+echo "Mounted $1 to $TARGET"
