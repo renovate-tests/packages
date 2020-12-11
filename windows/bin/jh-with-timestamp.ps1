790bfdd76b7480326d48410055558a0399eb027a
Jean Honlet <jehon@users.noreply.github.com>
2020-12-11 18:00:00 +0100

    feature: jh-with-timestamp

---
+++ windows/bin/jh-with-timestamp.ps1
@@ -0,0 +1,21 @@
+﻿
+Param([String]$path)
+
+# Thanks to https://stackoverflow.com/a/1954384/1954789
+
+$dirName  = [io.path]::GetDirectoryName($path)
+if ($dirName) {
+    $dirName = $dirName + '\'
+}
+$filename = [io.path]::GetFileNameWithoutExtension($path)
+$ext      = [io.path]::GetExtension($path)
+$ts       = get-date -Format "yyyy-MM-dd hh-mm-ss"
+$newName  = "$filename-$ts$ext"
+
+echo "In: '$dirName'"
+echo "From: $filename$ext"
+echo "To:   $newName"
+echo "Rename-Item" "$path" "$dirName$newName"
+Copy-Item "$path" "$dirName$newName"
+
+pause
