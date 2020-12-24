2465ba3df7473b20beb705fad793c39c7b22a88b
Jean Honlet <jehon@users.noreply.github.com>
2020-12-24 18:00:00 +0100

    feat(nsi): fix some scripts

---
+++ windows/bin/jh-nsi-fix-dns.ps1
@@ -0,0 +1,6 @@
+
+$reg = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\DNSClient\DnsPolicyConfig'
+
+Get-ChildItem -Path $reg | ForEach-Object { Remove-Item $_.pspath }
+
+Restart-Service DNSCache -force
---
+++ windows/bin/jh-nsi-fix-users.ps1
@@ -0,0 +1,21 @@
+
+# must be at C:\ to work !  how bizarre
+
+Write-Output "Starting..."
+
+if (Test-Path "%WinDir%\System32\GroupPolicyUsers") {
+    Write-Output "Deleting GroupPolicyUsers"
+    Remove-Item -Recurse -Force "%WinDir%\System32\GroupPolicyUsers"
+}
+
+if (Test-Path "%WinDir%\System32\GroupPolicy") {
+    Write-Output "Deleting GroupPolicy"
+    Remove-Item -Recurse -Force "%WinDir%\System32\GroupPolicy"
+}
+
+Write-Output "GP Update..."
+gpupdate /force
+
+Write-Output "...done"
+
+pause
