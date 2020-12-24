9dc03895b22396eeb0f075e859f3b7ca95044107
Jean Honlet <jehon@users.noreply.github.com>
2020-12-24 18:00:00 +0100

    feat(screen): wip about child windows

--- windows/lib/screens.ps1
+++ windows/lib/screens.ps1
@@ -139,6 +139,92 @@ Function Set-Window-To-Screen {
     }
 }
 
+function Get-ChildWindow {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline = $true, ValueFromPipelinebyPropertyName = $true)]
+        [ValidateNotNullorEmpty()]
+        [System.IntPtr]$MainWindowHandle
+    )
+
+    BEGIN {
+        function Get-WindowName($hwnd) {
+            $len = [apifuncs]::GetWindowTextLength($hwnd)
+            if ($len -gt 0) {
+                $sb = New-Object text.stringbuilder -ArgumentList ($len + 1)
+                $rtnlen = [apifuncs]::GetWindowText($hwnd, $sb, $sb.Capacity)
+                $sb.tostring()
+            }
+        }
+
+        if (("APIFuncs" -as [type]) -eq $null) {
+            Add-Type  @"
+            using System;
+            using System.Runtime.InteropServices;
+            using System.Collections.Generic;
+            using System.Text;
+            public class APIFuncs
+              {
+                [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+                public static extern int GetWindowText(IntPtr hwnd,StringBuilder lpString, int cch);
+
+                [DllImport("user32.dll", SetLastError=true, CharSet=CharSet.Auto)]
+                public static extern IntPtr GetForegroundWindow();
+
+                [DllImport("user32.dll", SetLastError=true, CharSet=CharSet.Auto)]
+                public static extern Int32 GetWindowThreadProcessId(IntPtr hWnd,out Int32 lpdwProcessId);
+
+                [DllImport("user32.dll", SetLastError=true, CharSet=CharSet.Auto)]
+                public static extern Int32 GetWindowTextLength(IntPtr hWnd);
+
+                [DllImport("user32")]
+                [return: MarshalAs(UnmanagedType.Bool)]
+                public static extern bool EnumChildWindows(IntPtr window, EnumWindowProc callback, IntPtr i);
+                public static List<IntPtr> GetChildWindows(IntPtr parent)
+                {
+                   List<IntPtr> result = new List<IntPtr>();
+                   GCHandle listHandle = GCHandle.Alloc(result);
+                   try
+                   {
+                       EnumWindowProc childProc = new EnumWindowProc(EnumWindow);
+                       EnumChildWindows(parent, childProc,GCHandle.ToIntPtr(listHandle));
+                   }
+                   finally
+                   {
+                       if (listHandle.IsAllocated)
+                           listHandle.Free();
+                   }
+                   return result;
+               }
+                private static bool EnumWindow(IntPtr handle, IntPtr pointer)
+               {
+                   GCHandle gch = GCHandle.FromIntPtr(pointer);
+                   List<IntPtr> list = gch.Target as List<IntPtr>;
+                   if (list == null)
+                   {
+                       throw new InvalidCastException("GCHandle Target could not be cast as List<IntPtr>");
+                   }
+                   list.Add(handle);
+                   //  You can modify this to check to see if you want to cancel the operation, then return a null here
+                   return true;
+               }
+                public delegate bool EnumWindowProc(IntPtr hWnd, IntPtr parameter);
+               }
+"@
+        }
+    }
+
+    PROCESS {
+        foreach ($child in ([apifuncs]::GetChildWindows($MainWindowHandle))) {
+            Write-Output (, ([PSCustomObject] @{
+                        MainWindowHandle = $MainWindowHandle
+                        ChildId          = $child
+                        ChildTitle       = (Get-WindowName($child))
+                    }))
+        }
+    }
+}
+
 Function Move-Window-To-Screen {
     <#
         .SYNOPSIS
@@ -167,7 +253,7 @@ Function Move-Window-To-Screen {
 
         foreach ($t in $threads) {
             $wHandle = $t.MainWindowHandle
-            Write-Output "Setting $Name $uPid"
+            Write-Output "Setting $Name $wHandle"
 
             Write-Output "- Restore size"
             # Restore window (4)
@@ -191,7 +277,7 @@ Function Move-Window-To-Screen {
 
             # Activate the window (5)
             #Write-Output "- Activating it"
-            #[Win32.NativeMethods]::ShowWindow($t.MainWindowHandle, 5) | Out-Null
+            #[Win32.NativeMethods]::ShowWindow($wHandle, 5) | Out-Null
         }
     }
 }
