957d2b4320853792f36b232de1739f31837c8edc
Jean Honlet <jehon@users.noreply.github.com>
2020-12-24 18:00:00 +0100

    feat: requires-admin

--- windows/bin/jh-nsi-fix-dns.ps1
+++ windows/bin/jh-nsi-fix-dns.ps1
@@ -1,5 +1,10 @@
 
-#Requires -RunAsAdministrator
+. $PSScriptRoot\..\lib\require-admin.ps1
+
+if (Enter-Admin) {
+    Write-Output "This script is restated as admin"
+    Exit 0
+}
 
 $reg = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\DNSClient\DnsPolicyConfig'
 
--- windows/bin/jh-nsi-fix-users.ps1
+++ windows/bin/jh-nsi-fix-users.ps1
@@ -1,18 +1,23 @@
 
-#Requires -RunAsAdministrator
-
 # must be at C:\ to work !  how bizarre
+
+. $PSScriptRoot\..\lib\require-admin.ps1
+
+if (Enter-Admin) {
+    Write-Output "This script is restated as admin"
+    Exit 0
+}
 
 Write-Output "Starting..."
 
-if (Test-Path "%WinDir%\System32\GroupPolicyUsers") {
+if (Test-Path "$env:windir\System32\GroupPolicyUsers") {
     Write-Output "Deleting GroupPolicyUsers"
-    Remove-Item -Recurse -Force "%WinDir%\System32\GroupPolicyUsers"
+    Remove-Item -Recurse -Force "$env:windir\System32\GroupPolicyUsers"
 }
 
-if (Test-Path "%WinDir%\System32\GroupPolicy") {
+if (Test-Path "$env:windir\System32\GroupPolicy") {
     Write-Output "Deleting GroupPolicy"
-    Remove-Item -Recurse -Force "%WinDir%\System32\GroupPolicy"
+    Remove-Item -Recurse -Force "$env:windir\System32\GroupPolicy"
 }
 
 Write-Output "GP Update..."
