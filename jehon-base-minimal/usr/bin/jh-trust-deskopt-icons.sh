c0260873512606f8100ff6ae27ace08bdada8b75
Jean Honlet <jehon@users.noreply.github.com>
2021-01-02 16:03:37 +0100

    wip: trust desktop icon

--- jehon-base-minimal/usr/bin/jh-trust-deskopt-icons.sh
+++ jehon-base-minimal/usr/bin/jh-trust-deskopt-icons.sh
@@ -1,31 +1,18 @@
 #!/usr/bin/env bash
 
-gspid=$(pgrep "gnome-session")
-dbsavn="DBUS_SESSION_BUS_ADDRESS"
-dbsadd=""
-shortcutFile=""
-istrusted=0
+file="$1"
 
-echo "Desktop icon configurations..."
-if ! (( gspid )) ; then
-	echo "GNOME session not found"
+# Thanks to https://askubuntu.com/a/1152748/638656
+
+if ! type gio ; then
+	echo "goi not found"
 	exit 1
 fi
 
-dbsadd="$(grep -z "$dbsavn" "/proc/$gspid/environ")"
-if [[ "$dbsadd" = "" ]]; then
-	echo "environment variable '$dbsavn' not found"
-	exit 1
-fi
-
-eval "export $dbsadd"
-
-shortcutFile="$1"
-istrusted=$(gio info "$shortcutFile" | awk '$1 == "metadata::trusted:" && $2 == "yes" {print 1 ; exit}')
-if (( istrusted )) ; then
-	echo "Icon already trusted  $shortcutFile"
+if gio info "$file" | grep "metadata::trusted:" ; then
+	echo "Icon already trusted  $file"
 	exit 0
 fi
 
-echo "Trusting $shortcutFile"
-gio set "$shortcutFile" "metadata::trusted" "yes"
+echo "Trusting $file"
+gio set "$file" "metadata::trusted" "yes"
