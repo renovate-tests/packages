61b7e1790e4efa3e28ef84eb083b1e0a917da615
Jean Honlet <jehon@users.noreply.github.com>
2021-02-04 18:00:00 +0100

    doc: enrich jenkins setup

--- bin/jh-jenkins-setup.sh
+++ bin/jh-jenkins-setup.sh
@@ -8,3 +8,12 @@ chown -R jenkins /var/backups/jenkins
 usermod -a -G docker jenkins
 
 ufw allow 8080
+
+cp /etc/default/jenkins /etc/default/jenkins.bak
+sed -i "s/JENKINS_HOME=.*/JENKINS_HOME=\/home\/jenkins/" /etc/default/jenkins
+if [ -d /var/lib/jenkins ] ; then
+    mv /var/lib/jenkins /home
+else
+    mkdir /home/jenkins
+    chown jenkins /home/jenkins
+fi
