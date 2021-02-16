29f359a5512d30243390bbd4a4612ad39c023572
Jean Honlet <jehon@users.noreply.github.com>
2021-02-16 18:00:00 +0100

    docs: enrich jenkins install

--- bin/jh-jenkins-setup.sh
+++ bin/jh-jenkins-setup.sh
@@ -2,24 +2,29 @@
 
 # NOT TESTED
 
+# Backups
 mkdir -p /var/backups/jenkins
 chown -R jenkins /var/backups/jenkins
 
+# Allow agents to run docker
 usermod -a -G docker jenkins
 
-ufw allow 8080
+# Allow http access
+ufw allow Jenkins
 
 #
 # Change home to be in /home/jenkins
 #   for snaps
 #   thanks to https://dzone.com/articles/jenkins-02-changing-home-directory
 #
-
 cp /etc/default/jenkins /etc/default/jenkins.bak
 sed -i "s/JENKINS_HOME=.*/JENKINS_HOME=\/home\/jenkins/" /etc/default/jenkins
+
 if [ -d /var/lib/jenkins ] ; then
     mv /var/lib/jenkins /home
-else
-    mkdir /home/jenkins
-    chown jenkins /home/jenkins
 fi
+
+mkdir -p /home/jenkins
+chown jenkins /home/jenkins
+
+# End
