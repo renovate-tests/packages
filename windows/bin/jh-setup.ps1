934ffa99325e91a2f1aed7bec06b2fcc8935cf35
Jean Honlet <jehon@users.noreply.github.com>
2021-02-18 18:00:00 +0100

    feat: add jh-setup for chocolatey

---
+++ windows/bin/jh-setup.ps1
@@ -0,0 +1,23 @@
+﻿
+. $PSScriptRoot\..\lib\require-admin.ps1
+
+if (Enter-Admin) {
+    Write-Output "This script is restated as admin"
+    Exit 0
+}
+
+choco feature enable -n=allowGlobalConfirmation
+
+choco install git
+choco install docker-desktop
+choco install vcbuildtools
+choco install naps2
+choco install mobaxterm
+choco install hashtab
+choco install psutils
+
+# choco install meld
+# choco install gradle
+# choco install nano
+# choco install jdk11 -params "static=false"
+# choco install xmind
