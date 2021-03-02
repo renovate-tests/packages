6552330d2ff8a2101383daa0ac160113459d800e
Jean Honlet <jehon@users.noreply.github.com>
2021-03-02 18:00:00 +0100

    chore: remove logs in stat build docker image

--- dockers/jehon-docker-build/start.sh
+++ dockers/jehon-docker-build/start.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-echo "Should run as: $HOST_UID : $HOST_GID"
+# echo "Should run as: $HOST_UID : $HOST_GID"
 
 GN=$( grep ":$HOST_GID:" /etc/group | cut -d ":" -f 1 )
 if [ -z "$GN" ]; then
 	GN="host_group"
-	echo "Creating group $GN"
 	groupadd $GN -g "$HOST_GID"
 fi
 
 if ! id -u "$HOST_UID" >/dev/null 2>/dev/null ; then
 	UN="host_user"
-	echo "Creating user $UN"
 	adduser --system --uid "$HOST_UID" --gid "$HOST_GID" $UN
 else
 	UN="$(id -un "$HOST_UID")"
@@ -22,7 +20,7 @@ if ! grep "$UN" /etc/sudoers ; then
 	echo "$UN    ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers
 fi
 
-echo "Going with user $UN and group $GN"
-echo "Running in bash: '$*'"
+# echo "Going with user $UN and group $GN"
+# echo "Running in bash: '$*'"
 
 runuser -u "$UN" -- bash -c "$*"
