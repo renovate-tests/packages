ebfc650aaf8bf81c70b2a0692f9d30da3e1c21ee
Jean Honlet <jehon@users.noreply.github.com>
2021-03-12 18:55:17 +0100

    feat: fix scan

--- jehon-base-minimal/usr/bin/jh-scan-to-indexed.sh
+++ jehon-base-minimal/usr/bin/jh-scan-to-indexed.sh
@@ -2,9 +2,14 @@
 
 set -e
 
+OPT="-x $1 -y $2"
+
 case "$1" in
 	"10x15" )
 		OPT="-x 108 -y 152"
+		;;
+	"17x10" )
+		OPT="-x 170 -y 101"
 		;;
 	"15x10" )
 		OPT="-x 151 -y 101"
@@ -15,16 +20,27 @@ case "$1" in
 	"14x9" )
 		OPT="-x 140 -y 90"
 		;;
+	"13x10" )
+		OPT="-x 130 -y 100"
+		;;
 	"15x21" )
 		OPT="-x 150 -y 210"
 		;;
 	"s" )
 		OPT="-x 200 -y 195"
 		;;
-	*)
-		echo "Unsupported"
-		exit 1
+	"a4" )
+		OPT="-x 205 -y 295"
+		;;
+	"a5" )
+		OPT="-x 205 -y 135"
 esac
+
+if [ -z "$OPT" ]; then
+	echo "Unsupported"
+	exit 1
+fi
+
 # Transform into an array
 read -r -a OPT <<< "$OPT"
 
