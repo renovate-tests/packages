1f77c8bc338bbf45ccaac12c32afe291b28555a9
Jean Honlet <jehon@users.noreply.github.com>
2021-05-07 13:19:20 +0200

    feat: jh-run-and-capture

--- tests/shell/test-jh-run-and-capture.sh
+++ tests/shell/test-jh-run-and-capture.sh
@@ -11,10 +11,13 @@ JH_ROOT="$( dirname "$( dirname "$( dirname "$( dirname "${BASH_SOURCE[0]}" )" )
 test_capture_empty
 
 test_capture "run successfull" jh-run-and-capture bash -c "echo coucou"
-assert_captured_success
-assert_equals "No output" "" "$JH_TEST_CAPTURED_OUTPUT"
+assert_captured_success "Should success"
+assert_captured_output_contains "run failure show command" "echo coucou"
+# assert_true "Output only ok" "" "$JH_TEST_CAPTURED_OUTPUT"
 
 test_capture "run failure" jh-run-and-capture bash -c "echo coucou; nothing"
+assert_captured_failure "Shoud fail"
 assert_equals "run failure exit code" "$JH_TEST_CAPTURED_EXITCODE" "127"
+assert_captured_output_contains "run failure show command" "echo coucou; nothing"
 assert_captured_output_contains "run failure show stdout" "coucou"
-assert_captured_output_contains "run failure show stdout" "nothing:"
+assert_captured_output_contains "run failure show stderr" "nothing:"
